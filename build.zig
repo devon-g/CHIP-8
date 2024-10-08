041a461b3aef1d25217d81764a7ca78866540e72
Devon Gardner <devon@goosur.com>
2024-10-07 21:34:29 -0400

    migrate from cmake to zig build

--- build.zig
+++ build.zig
@@ -34,4 +34,12 @@ pub fn build(b: *std.Build) void {
     exe.linkLibCpp();
 
     b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    if (b.args) |args| {
+        run_exe.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
 }
